9de046185d5f592e066ead443ab23b43d4782e06
Jay <jvelayud@gmail.com>
2019-06-07 19:55:35 -0400

    Test code

--- handler.js
+++ handler.js
@@ -1,8 +1,7 @@
 //import AWS from "aws-sdk";
-const a = null;
+//const a = null;
 
 export const hello = async (event, context) => {
-  console.log(a);
   //console.log(a.b);
   //AWS.config.update({ region: "us-east-1" });
   return {
